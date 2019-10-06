e27e0010073753673e2d005a661da313b07934f4
Mudit7 <muditmalpani7@gmail.com>
2019-10-06 17:15:55 +0530

    sorted files generated, before kway merge

--- Assign3/KWaySort.cpp
+++ Assign3/KWaySort.cpp
@@ -1,4 +1,5 @@
 #include</Users/mudit/stdc++.h>
+#include <sys/stat.h>
 
 using namespace std;
 typedef long long int ll;
@@ -19,7 +20,6 @@ class MinHeap{
     {
         return 0;    
     }
-
     void heapify(int node)
     {
 
@@ -47,8 +47,9 @@ class MinHeap{
 ll getFileSize(string filename)
 {
     ll size=0;
-
-    return size;
+    struct stat stat_buff;
+    int rc = stat(filename.c_str(), &stat_buff);
+    return rc == 0 ? stat_buff.st_size : -1;
 }
 
 vi getKthChunk(string fname,int k,int size)
@@ -56,7 +57,13 @@ vi getKthChunk(string fname,int k,int size)
     vi chunk(size,0);
     int offset=k*size;
     //fseek(offset) then read then rewind
-
+    FILE *f=fopen(fname.c_str(),"r");
+    int i=0;
+    while(i<size){
+    fread(&chunk[i],4,1,f);
+    i++;
+    }
+    fclose(f);
     return chunk;
 }
 void mergeKSortedFile(string fname,int k,int chunkSize)
@@ -65,9 +72,12 @@ void mergeKSortedFile(string fname,int k,int chunkSize)
     
 }
 
-void storeInFiles(vi chunk,int i)
+void storeInFiles(vi chunk,int k)
 {
-
+    FILE *f=fopen(to_string(k).c_str(),"w");
+    for(int i=0;i<chunk.size();i++)
+    fprintf(f,"%d",chunk[i]);
+    fclose(f);
 }
 
 int main(int argc,char* argv[])
@@ -78,15 +88,14 @@ int main(int argc,char* argv[])
     MinHeap heap(k);
     string input_file=argv[1];
     string output_file=argv[2];
-    int filesize=1000;
+    //int filesize=1000;
     ll chunkSize=getFileSize(input_file)/k;    
     vi unsorted_chunk(chunkSize,0);
     vi sorted_chunk(chunkSize,0);
     //create a new file to store k number of sorted chunks
     //fread data=chunksize
-    FILE *fin=fopen(argv[1],"r");
-    FILE *fout=fopen(argv[2],"w");
     //FILE f2=fopen("temp_output_file_name","rb");
+    //cout<<chunkSize;
     for(int i=0;i<k;i++)
     {
         //store in ith file
